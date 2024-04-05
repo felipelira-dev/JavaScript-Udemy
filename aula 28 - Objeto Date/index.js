6dc33dfa8a2ae5f34c6c73aa2a36d8b3a8a988b1
felipelira-dev <163548758+felipelira-dev@users.noreply.github.com>
2024-04-04 23:24:00 -0300

    att

--- aula 28 - Objeto Date/index.js
+++ aula 28 - Objeto Date/index.js
@@ -1,3 +1,4 @@
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
-const data = new Date(0); //date 0 01/01/1970 *Timestamp Unix || Época Unix*
-console.group(data.toString());
+const tresHoras = 60 * 60 * 3 * 1000
+const data = new Date(0);
+console.log(data.toString());
