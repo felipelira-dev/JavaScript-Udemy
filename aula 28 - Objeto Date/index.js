56e4afa9b68b66a9c6d67e931932174414beb348
felipelira-dev <163548758+felipelira-dev@users.noreply.github.com>
2024-04-04 23:26:37 -0300

    att

--- aula 28 - Objeto Date/index.js
+++ aula 28 - Objeto Date/index.js
@@ -1,4 +1,5 @@
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Date
-const tresHoras = 60 * 60 * 3 * 1000
-const data = new Date(0);
+const tresHoras = 60 * 60 * 3 * 1000;
+const umDia = 60 * 60 * 24 * 1000;
+const data = new Date(0 + tresHoras - umDia);  //01/01/1970 Timestamp unix ou Época unix
 console.log(data.toString());
